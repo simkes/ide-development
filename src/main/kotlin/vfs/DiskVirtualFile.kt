cce22b6af810d95c89e4663d9a229a5888471e43
Mikhail Rodionychev <tralllgm@gmail.com>
2023-11-17 00:16:52 +0100

    rename impl disk file

--- src/main/kotlin/vfs/DiskVirtualFile.kt
+++ src/main/kotlin/vfs/DiskVirtualFile.kt
@@ -4,7 +4,7 @@ import java.nio.file.Path
 import kotlin.io.path.inputStream
 import kotlin.io.path.outputStream
 
-class PhysicalDiskFile(private val path: Path, private val vfs: VirtualFileSystem) : VirtualFile {
+class DiskVirtualFile(private val path: Path, private val vfs: VirtualFileSystem) : VirtualFile {
     override lateinit var contents: ByteArray
     override fun getName() = path.fileName.toString()
 
